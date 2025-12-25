229048161f55736f1c5be92cf2e518a344bab696
RANA KRISH BHARATBHAI <krishrana359@gmail.com>
2025-12-25 16:24:22 +0530

    Delete crawl directory

--- crawl/sitemap.py
+++
@@ -1,8 +0,0 @@
-import requests
-def scan(domain, save_path):
-    try:
-        r = requests.get(f"http://{domain}/sitemap.xml")
-        if r.status_code==200:
-            with open(f"{save_path}/sitemap.xml", "w") as f: f.write(r.text)
-    except: pass
-      
